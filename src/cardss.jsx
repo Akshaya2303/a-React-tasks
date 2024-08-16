0202589eb48d82881d8129a645a9d65af5a1d7dd
Akshaya2303 <akshayanayakidi@gmail.com>
2024-08-16 18:11:53 +0530

    tables,cards

---
+++ src/cardss.jsx
@@ -0,0 +1,25 @@
+const Cardss=()=>{
+    return(
+        <>
+        <div>
+            <h1>Cardss</h1>
+            <div className="food">
+                <div>
+                <img src="https://th.bing.com/th/id/OIP.aMYg-LP-qBIn8RHS-1DDCQAAAA?w=474&h=315&rs=1&pid=ImgDetMain"></img>
+                 <p>food</p>
+            <p>indian food</p>
+            </div>  <div>
+                <img src="https://nomadparadise.com/wp-content/uploads/2021/04/new-zealand-food-14.jpg"></img>
+            <p>chicken</p>
+            <p>indian food</p></div>
+            <div>
+                <img src="https://static.skipthedishes.com/main-garden-chinese-restaurant-menu-image-large-1527692486500.jpg"></img>
+            <p>noodles</p>
+            <p>chineese food</p>
+            </div></div>
+        </div>
+        </>
+    )
+
+}
+export default Cardss
